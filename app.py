2a66edfaa57604c5b0be8071c0d7de1f00bbc635
Joyce-Lillian <31736419+Joyce-Lillian@users.noreply.github.com>
2021-06-19 13:29:57 -0400

    Full sentence and punctuation
    Updated app.py to match collab with returning the full sentence and punctuation

--- app.py
+++ app.py
@@ -71,8 +71,31 @@ def answer_question(question, answer_text):
     # Get the string versions of the input tokens.
     tokens = tokenizer.convert_ids_to_tokens(input_ids)
 
-    # Start with the first token.
-    answer = tokens[answer_start]
+    answer = full_sentence(tokens, answer_start, answer_end)
+
+    print('Answer: "' + answer + '"')
+
+
+def start_of_sentence(tokens, answer_start, answer_end):
+
+    # Start with the first token and/or first letter in sentence
+    if tokens[answer_start-1] == '.' or tokens[answer_start] == None:
+        answer = tokens[answer_start]
+
+    # Otherwise keep searching for the beginning of the sentence
+    else:
+        while tokens[answer_start] != '.':
+            answer_start -= 1
+
+    # Find the last letter in the sentence
+    if tokens[answer_end] != '.' or tokens[answer_end] != None:
+        while tokens[answer_end] != '.' or answer_end+1 >= len(tokens):
+            answer_end += 1
+
+    return tokens[answer_start], answer_start, answer_end
+
+
+def update_tokens(answer, tokens, answer_start, answer_end):
 
     # Select the remaining answer tokens and join them with whitespace.
     for i in range(answer_start + 1, answer_end + 1):
@@ -81,19 +104,55 @@ def answer_question(question, answer_text):
         if tokens[i][0:2] == '##':
             answer += tokens[i][2:]
 
+        # If there is a punctuation mark, then combine without space
+        elif tokens[i].isalnum() == False:
+            answer += tokens[i]
+
         # Otherwise, add a space then the token.
         else:
             answer += ' ' + tokens[i]
 
-    print('Answer: "' + answer + '"')
+    return answer
 
 
-# bert_abstract = "We introduce a new language representation model called BERT, which stands for Bidirectional Encoder Representations from Transformers. Unlike recent language representation models (Peters et al., 2018a; Radford et al., 2018), BERT is designed to pretrain deep bidirectional representations from unlabeled text by jointly conditioning on both left and right context in all layers. As a result, the pre-trained BERT model can be finetuned with just one additional output layer to create state-of-the-art models for a wide range of tasks, such as question answering and language inference, without substantial taskspecific architecture modifications. BERT is conceptually simple and empirically powerful. It obtains new state-of-the-art results on eleven natural language processing tasks, including pushing the GLUE score to 80.5% (7.7% point absolute improvement), MultiNLI accuracy to 86.7% (4.6% absolute improvement), SQuAD v1.1 question answering Test F1 to 93.2 (1.5 point absolute improvement) and SQuAD v2.0 Test F1 to 83.1 (5.1 point absolute improvement)."
+def update_punctuation(answer):
 
-bert_abstract = "Purchased Content. When you purchase an item of content, your content will be stored in a digital locker and you may view it an unlimited number of times for during your Locker Period. The “Locker Period” will be for at least 5 years from the date of your purchase(subject to the restrictions described in the YouTube Paid Service Terms of Service). Each item of purchased content may have a different Locker Period and you agree to the Locker Period before you order it. Pausing, stopping, or rewinding purchased content will not extend the Locker Period.  As noted in the YouTube Paid Service Terms of Service, if an item of purchased content becomes unavailable during the five year period from the purchase date, you may request a refund."
+    # Set beginning of sentence to a letter
+    while answer[0].isalnum() == False:
+        answer = answer[1:]
 
-question = "Can I get a refund??"
+    # Set end of sentence to a letter
+    len_answer = len(answer)
+    while answer[len_answer-1].isalnum() == False:
+        answer = answer[:len_answer-1]
+        len_answer -= 1
+
+    return answer
+
+
+def full_sentence(tokens, answer_start, answer_end):
+
+    # Update indices to include full sentence
+    new_res = start_of_sentence(tokens, answer_start, answer_end)
+    answer = new_res[0]
+    answer_start = new_res[1]
+    answer_end = new_res[2]
+
+    # Update answer to include correct spacing with punctuation and combine ## tokens
+    answer = update_tokens(answer, tokens, answer_start, answer_end)
+
+    # Remove extraneous punctuation from start and end
+    answer = update_punctuation(answer)
+
+    # Capitalize the first letter and add period punctuation to end
+    return answer[0].upper()+answer[1:]+'.'
+
+
+bert_abstract = "Purchased Content. When you purchase an item of content, your content will be stored in a digital locker and you may view it an unlimited number of times for during your Locker Period. The “Locker Period” will be for at least 5 years from the date of your purchase (subject to the restrictions described in the YouTube Paid Service Terms of Service). Each item of purchased content may have a different Locker Period and you agree to the Locker Period before you order it. Pausing, stopping, or rewinding purchased content will not extend the Locker Period.  As noted in the YouTube Paid Service Terms of Service, if an item of purchased content becomes unavailable during the five year period from the purchase date, you may request a refund."
+
+question = "Can I get a refund?"
 
 answer_question(question, bert_abstract)
+
 
 print("hello world")
